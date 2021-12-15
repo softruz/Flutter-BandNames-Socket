81a17effd2eab45d79ee507a01c8bbe83273bdae
softruz@gmail.com <softruz@gmail.com>
2021-12-15 20:50:02 +0100

    Fin sección 2

---
+++ lib/models/band.dart
@@ -0,0 +1,11 @@
+class Band {
+  String id;
+  String name;
+  int votes;
+
+  Band({required this.id, required this.name, required this.votes});
+
+  // factory constructor
+  factory Band.fromMap(Map<String, dynamic> obj) =>
+      Band(id: obj['id'], name: obj['name'], votes: obj['votes']);
+}
